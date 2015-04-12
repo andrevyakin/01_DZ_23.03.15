fc0ae78d41a3166cf8d71a0391ce1f60969a932f
andrevyakin <andrevyakin@gmail.com>
2015-04-12 12:09:00 +0300

    Второй вариант создания объекта

--- DZ/01_DZ/Script.js
+++ DZ/01_DZ/Script.js
@@ -30,7 +30,7 @@ function MyRectangle (rectangle, xh, yh, xb, yb)
     function Show ()
     {
         document.write("Прямоугольник: " + rectangle + "<br/>");
-        document.write("Координаты: ", xh + ", " + yh + ", " + xb + ", " + yb + "." + "<br/>")
+        document.write("Координаты: " + xh + ", " + yh + ", " + xb + ", " + yb + "." + "<br/>");
     }
 }
 
@@ -52,6 +52,59 @@ function Run()
     mas[0].Show();
 
 }
+
+// или так:
+
+//Объект "Прямоугольник"
+var MyRectangle1 = {
+
+    constructor: function Constructor  (rectangle, xh, yh, xb, yb) {
+        //свойства
+        this.rectangle = rectangle;
+        this.xh = xh;
+        this.yh = yh;
+        this.xb = xb;
+        this.yb = yb;
+
+       return this;
+    },
+
+    //методы
+
+    GetData: function ()
+    {
+        this.rectangle = prompt("Введите название (идентификатор) прямоугольника.", "");
+        this.xh = prompt( "Введите координаты Х верхнего левого угла.", "");
+        this.yh = prompt( "Введите координаты Y верхнего левого угла.", "");
+        this.xb = prompt( "Введите координаты Х нижнего правого угла.", "");
+        this.yb = prompt( "Введите координаты Y нижнего правого угла.", "");
+    },
+
+    Show: function ()
+    {
+        document.write("Прямоугольник: " + this.rectangle + "<br/>");
+        document.write("Координаты: " + this.xh + ", " + this.yh + ", " + this.xb + ", " + this.yb + "." + "<br/>");
+    }
+};
+
+function Run1()
+{
+    //объект с параметрами
+    var rect1 = Object.create(MyRectangle1).constructor("1-й прямоугольник", 10, 20, 30, 40);
+    rect1.Show();
+
+    //Объект без параметров
+    var rect2 = new Object(MyRectangle1);
+    rect2.GetData();
+    rect2.Show();
+
+    //Массив объектов прямоугольник
+    var mas = new Array(rect1, rect2);
+    mas[0].Show();
+
+}
+
+
 
 
 
